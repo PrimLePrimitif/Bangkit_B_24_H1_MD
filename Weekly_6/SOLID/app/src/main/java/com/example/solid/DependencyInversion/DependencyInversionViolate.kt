dae176aae95804bdf28dfd35efd99e40ca9b43ab
PrimLePrimitif <Pratamacompeteros@gmail.com>
2024-03-30 09:47:12 +0700

    Add Solid Mat

---
+++ Weekly_6/SOLID/app/src/main/java/com/example/solid/DependencyInversion/DependencyInversionViolate.kt
@@ -0,0 +1,50 @@
+package com.example.solid.DependencyInversion
+
+class AxeViolate {
+    fun attack() {
+        println("Attack with Axe")
+    }
+}
+
+class SwordViolate {
+    fun attack() {
+        println("Attack with sword")
+    }
+}
+
+class PlayerViolate {
+    var name: String = "Primitf"
+
+    // Wkwk yakin, mau implementasi kaku kek gini ?
+    // Gimana kalau mau add senjata lain ? Harus variable baru kah ??
+    var weaponSword: SwordViolate? = null
+    var weaponAxe: AxeViolate? = null
+
+    fun setWeapon(weaponSword: SwordViolate) {
+        this.weaponSword = weaponSword
+    }
+
+    fun setWeapon(weaponAxe: AxeViolate) {
+        this.weaponAxe = weaponAxe
+    }
+
+    fun printInfo() {
+        println("Player name ${this.name}")
+        println("Current weapon : ")
+        if(this.weaponAxe != null) {
+            println("Axe")
+        } else if(this.weaponSword != null) {
+            println("Sword")
+        }
+    }
+}
+
+fun main() {
+    val player: PlayerViolate = PlayerViolate()
+    val axe : AxeViolate = AxeViolate()
+    val sword: SwordViolate = SwordViolate()
+    player.setWeapon(axe)
+    player.setWeapon(sword)
+    player.printInfo()
+
+}
